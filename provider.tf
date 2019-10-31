5e43bef042bb36b373f7b90bdff730ddb92e2da0
Giancarlo Tupac Gutierrez <giancarlo.tupac@gmail.com>
2019-10-31 14:27:29 -0500

    prueba 5

--- provider.tf
+++ provider.tf
@@ -3,7 +3,6 @@
 #---------------------------------------------------------
 
 variable ibmcloud_api_key {
-  default = "3xlibMHImbejbAQLdu1zaWyArejAwAyuxul8TeeLu1tZ"
   description = "Ingresa el api key: 3xlibMHImbejbAQLdu1zaWyArejAwAyuxul8TeeLu1tZ"
 } 
 
