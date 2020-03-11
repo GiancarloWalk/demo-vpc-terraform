c7c1834f5cf186a2417031a74fd442dde00eb647
Giancarlo Tupac Gutierrez <47062199+GiancarloWalk@users.noreply.github.com>
2020-03-11 13:08:45 -0500

    no more ssh keys!!!

--- ssh.tf
+++ ssh.tf
@@ -1,9 +1,12 @@
 #---------------------------------------------------------
 # Create sshkey from file
 #---------------------------------------------------------
-resource "ibm_is_ssh_key" "sshkey" {
-  name       = "vpc-ssh-key"
-  public_key = "${file(var.ssh_public_key)}"
-  #No me permite guardarlo en un grupo de recursos específico vía Terraform, lo guarda en el rg por defecto
-}
+
+
+
+#resource "ibm_is_ssh_key" "sshkey" {
+#  name       = "vpc-ssh-key"
+#  public_key = "${file(var.ssh_public_key)}"
+#  #No me permite guardarlo en un grupo de recursos específico vía Terraform, lo guarda en el rg por defecto
+#}
 
